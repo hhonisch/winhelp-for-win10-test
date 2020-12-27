41801eee0064b728eb0359ecc073e12add45ea61
Hartmut Honisch <Hartmut.Honisch@gmail.com>
2020-12-27 20:51:22 +0100

    Fix problems shown by VS Powershell extension

--- build/github_build_helper.ps1
+++ build/github_build_helper.ps1
@@ -1,5 +1,5 @@
-dir env:
+Get-ChildItem env:
 
 # Extract version
 $my_version = $env:GITHUB_REF -replace "refs/tags/v",""
-echo "MY_VERSION=$my_version" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
+Write-Output "MY_VERSION=$my_version" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
