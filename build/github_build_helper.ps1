faaa69822541160ad9a25900592b18b330948d8e
Hartmut Honisch <Hartmut.Honisch@gmail.com>
2021-05-26 22:25:48 +0200

    Cleanup

--- build/github_build_helper.ps1
+++ build/github_build_helper.ps1
@@ -101,21 +101,3 @@ function Main() {
 
 
 Main
-
-
-
-
-
-
-
-
-
-
-
-<#
-Get-ChildItem env:
-
-# Extract version
-$my_version = $env:GITHUB_REF -replace "refs/tags/v",""
-Write-Output "MY_VERSION=$my_version" | Out-File -FilePath $env:GITHUB_ENV -Encoding utf8 -Append
-#>
